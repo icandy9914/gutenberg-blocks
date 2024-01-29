5308924d6f02787ed4bd828ce5c188b3cfa27122
Ganesh Vishwakarma <it.ganeshvishwakarma@gmail.com>
2024-01-29 12:20:56 +0530

    First commit

---
+++ icandy-myfirst-gt-block.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package icandymygtblock
+ */
+
+/*
+/*
+ * Plugin Name: Icandy First Gutenberg Block
+ * Plugin URI: https://icandevelop.com/plugins/phonepe-by-icandevelop
+ * Description: My first Guterberg block
+ * Author: I Can Develop
+ * Author URI: https://icandevelop.com
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: icandy-gt-block
+ */
+
+ // If the file is called directly! abort !!
+defined('ABSPATH') or die ("Hey Dud! You are not in the right place.");
